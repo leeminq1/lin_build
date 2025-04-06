79760a645c0fee27a03694264780bcbd51edc07f
leeminq1 <70461938+leeminq1@users.noreply.github.com>
2025-04-06 22:13:43 +0900

    마지막4

--- app/src/main/java/com/lindo/api/About.java
+++ app/src/main/java/com/lindo/api/About.java
@@ -19,7 +19,7 @@ import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
